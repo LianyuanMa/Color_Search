7c5c017ef787b4b06a2dd3c37eb002e39a357365
Lianyuan Ma <ucbql04@ucl.ac.uk>
2021-05-01 16:17:05 +0100

    deleting unused packages & configuring requirements.in

--- api/app/Routers/server.py
+++ api/app/Routers/server.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-from fastapi import  Request
 from fastapi import  Header
 from typing import Optional
 import requests
@@ -12,9 +11,13 @@ router = APIRouter()
 
 def extract_colour(image): #split the image into three: red, green, blue. Then merge the color histogram in a dictionary of lists which have int elements.
     rgbdict = {}
-    for colour, colour_name in zip(image.split(), ["red", "green", "blue"]): #We need to zip the two lists, otherwise the for loop will regard the two lists as two elements of a new list.
-        n,bins = np.histogram(colour, bins=256)  # the return value bins is the width list of columns(from 0 to 256-1). We still need a sorted list.
-        int_list_n = [int(i) for i in n]    #n is in numpy.ndarrrary type. Zipping the two list will have elements as numpy.int64, which can not be transfered into json. Variable type conversion is required.
+    for colour, colour_name in zip(image.split(), ["red", "green", "blue"]):
+        #We need to zip the two lists, otherwise the for loop will regard the two lists as two elements of a new list.
+        n,bins = np.histogram(colour, bins=256)
+        # the return value bins is the width list of columns(from 0 to 256-1). We still need a sorted list.
+        int_list_n = [int(i) for i in n]
+        #n is in numpy.ndarrrary type. Zipping the two list will have elements as numpy.int64, which can not be
+        # transfered into json. Variable type conversion is required.
         rgbdict[colour_name] = dict(zip(range(256), int_list_n)) 
     return rgbdict
 
