053f6c759a9cf0f227ab5193a5aae4023fbdd80a
harrisonpim <harrison.stroma@gmail.com>
2021-07-12 14:42:02 +0100

    adds a structure for processing images from ES with the local API

---
+++ process_images/main.py
@@ -0,0 +1,7 @@
+# connect to local Elasticsearch index
+# create a new index for the experiment
+
+# for each image ID in your primary index
+#   retrieve the thumbnail URL
+#   get the colours in the thumbnail using your API
+#   post the colour data for that ID to your new index
